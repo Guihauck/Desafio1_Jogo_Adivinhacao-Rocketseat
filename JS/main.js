af45e4a28358e8bf6d862545b54649882c7b05e0
Guihauck <guihauck1998@gmail.com>
2024-07-02 21:07:47 -0300

    Refatoração final do código e adição de novas funções para fácil leitura e resumo do código

--- JS/main.js
+++ JS/main.js
@@ -1,35 +1,45 @@
 // Variáveis
-const randomNumber = Math.round(Math.random()*10);
+let randomNumber = Math.round(Math.random() * 10);
 const video1 = document.querySelector(".video1");
 const video2 = document.querySelector(".video2");
 const tryButton = document.querySelector("#btnTry");
 const tryAgainButton = document.querySelector("#btnTryAgain");
 let xAgains = 1;
 
-// Eventos
-
+// Funções ou métodos (callback)
 const scriptNumberTry = (event) => {
   event.preventDefault(); //Não submeter o formulário.
   const numberInput = document.querySelector("#inputNumber");
 
   if (Number(numberInput.value) == randomNumber) {
-    video1.classList.add('hide');
-    video2.classList.remove('hide');
-
-    document.querySelector(".video2 h2")
-    .innerHTML = `Acertou! E suas tentativas foram ${xAgains}`
+    toggleScreen()
+    video2.querySelector(".video2 h2").innerHTML = `Acertou! E suas tentativas foram ${xAgains}`
   }
-  xAgains++
   numberInput.value = "";
+  xAgains++
 }
 
-tryButton.addEventListener('click', scriptNumberTry); 
-tryAgainButton.addEventListener('click', scriptNumberAction);
-
-// Funções
-
-function scriptNumberAction () {
-  video1.classList.remove('hide');
-  video2.classList.add('hide');
+function scriptNumberActionReset() {
+  toggleScreen()
+  randomNumber = Math.round(Math.random() * 10);
   xAgains = 1;
 }
+
+function toggleScreen() {
+  video1.classList.toggle('hide');
+  video2.classList.toggle('hide');
+}
+
+function enterAtivation () {
+  if (eventEnter.key == 'Enter' && video1.contains('hide')) {
+    scriptNumberActionReset();
+  }
+}
+
+// Eventos
+tryButton.addEventListener('click', scriptNumberTry);
+tryAgainButton.addEventListener('click', scriptNumberActionReset);
+document.addEventListener('keydown', enterAtivation)
+
+
+
